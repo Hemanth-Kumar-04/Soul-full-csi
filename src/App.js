c036819e2257bb1e7a1b8e6e54f96108adb8e379
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2024-03-10 02:55:48 +0530

    routerdom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
 import NotesWeb from './NotesWeb'
 import Consult from './Consult/Consult'
 import Doctors from './Doctor/Doctors'
 const App = () => {
   return (
-    <div>
-      <Doctors/>
-    </div>
+
+
+      
+<BrowserRouter>
+    
+    <Routes>
+      <Route path='/consult' element={<Consult/>} />
+      <Route path='/doctors' element={<Doctors/>} />
+      <Route path='/journal' element={<NotesWeb/>} />
+    </Routes>
+    </BrowserRouter>
+
   )
 }
 
 export default App
+
