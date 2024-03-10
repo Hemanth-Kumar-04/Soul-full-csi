f1327aee960e91fb503b22c609c7d5418639aabe
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2024-03-10 05:42:11 +0530

    full

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import React from "react";
 import NotesWeb from "./NotesWeb";
 import Consult from "./Consult/Consult";
 import Doctors from "./Doctor/Doctors";
+import Navbar from "./navbar/Navbar";
+
 const App = () => {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
+     
         <Route path="/consult" element={<Consult />} />
         <Route path="/doctors" element={<Doctors />} />
         <Route path="/journal" element={<NotesWeb />} />
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from 'react-router-dom'
+function Navbar() {
+  return (
+    <div className="Navbar-container">
+      <div className="Navbar-left">
+        <h3>SoulFull</h3>
+      </div>
+      <div className="Navbar-right">
+        <Link to={{ pathname: "/" }}>Home</Link>
+        <Link to={{ pathname: "/journal" }}>Journal</Link>
+        <Link to={{ pathname: "/consult" }}>Consult</Link>
+        <Link to={{ pathname: "/doctors" }}>Doctors</Link>
+        <a href="#" className="login">
+          Login{" "}
+        </a>
+        <div className="Navbar-Login"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
